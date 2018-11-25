044ec32bdcd0e7f5cdd86a62820fc1e617ed93d9
agrochmal <agrochmal@gmail.com>
2018-11-25 17:47:41 +0100

    Project initialization

---
+++ src/main/java/pl/adriangrochmal/autorepairshop/AutoRepairShopApplication.java
@@ -0,0 +1,12 @@
+package pl.adriangrochmal.autorepairshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRepairShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRepairShopApplication.class, args);
+    }
+}
